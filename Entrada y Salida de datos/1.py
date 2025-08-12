25deb0fcb1d6e8c7ded73579054fff2f86245db8
El Profe Rodo <elproferodofer@gmail.com>
2025-08-12 13:14:32 -0400

    Create 1.py

---
+++ Entrada y Salida de datos/1.py
@@ -0,0 +1,13 @@
+"""
+Calcular el promedio de cinco calificaciones, utilizando una variable por calificación y otra para el resultado del promedio. Debe mostrar el resultado por pantalla.
+"""
+
+nota1 = 5
+nota2 = 6.1
+nota3 = 4.2
+nota4 = 4
+nota5 = 7
+
+promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+
+print(f"El promedio es {promedio}")
