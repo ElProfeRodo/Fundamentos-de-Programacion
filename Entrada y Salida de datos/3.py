670ff1249b75f9b06cb4584015ea7eb4f67b54aa
El Profe Rodo <elproferodofer@gmail.com>
2025-08-12 13:54:14 -0400

    Create 3.py

---
+++ Entrada y Salida de datos/3.py
@@ -0,0 +1,10 @@
+"""
+Calcular índice de masa corporal (IMC) según la fórmula: peso en kilogramos dividido por estatura en metros elevada a dos. El peso y la estatura deben ser ingresados por el usuario. Mostrar por pantalla el resultado.
+"""
+
+peso = int(input("Ingresa tu peso: "))
+estatura = float(input("Ingresa tu estatura: "))
+
+imc = peso / (estatura ** 2)
+
+print(f"Tu IMC es {imc}")
