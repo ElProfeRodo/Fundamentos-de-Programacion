aa7ef38d95776559dbcbe1186ed01f29cdda6899
El Profe Rodo <elproferodofer@gmail.com>
2025-08-19 13:57:03 -0400

    Update 1.py

--- Estructuras de Decisión/1.py
+++ Estructuras de Decisión/1.py
@@ -1,1 +1,12 @@
-None
+"""
+Solicitar un número por el teclado y verificar si es positivo (mayor que cero). Mostrar el resultado de la comprobación por pantalla.
+"""
+
+numero = int(input("Ingresa un numero: "))
+
+if numero > 0:
+    print("El número es positivo")
+elif numero < 0:
+    print("El número es negativo")
+else:
+    print("El número es cero")
